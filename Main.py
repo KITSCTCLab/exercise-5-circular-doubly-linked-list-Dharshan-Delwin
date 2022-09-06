9b5bc86f1bba223870a7e1312048b23277bacad9
Dharshan-Delwin <110010704+Dharshan-Delwin@users.noreply.github.com>
2022-09-06 11:46:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,67 +1,85 @@
+
 class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.previous = self
-        self.next = self
+  def __init__(self,data):
+    self.data = data
+    self.next = None
+
+class Link:
+  def __init__(self):
+    self.head = None
+    self.count = 0
+  
+  def push(self,data):
+    new = Node(data)
+    new.next = self.head
+    self.head = new
+    self.count += 1
+  
+  def search(self,target):
+    n = self.head
+    result = 0
+    node_number = 0
+    new = {}
+    for i in range(self.count):
+      if(n.data == target):
+        new = {
+            "postion:":i,
+            "node value:":target
+        }
+    print()
+    return new
+
+        
+
+  def pair(self):
+    n = self.head
+    while(n):
+      print(n.data,end = "->")
+      n = n.next
+    print("\n")
+    
+  def delete_beg(self):
+    temp = self.head
+    self.head = self.head.next
+    temp = None
+  
+  def delete_end(self):
+    for i in range(self.count):
+      if(i == self.count - 1):
+        temp = self.head
+        self.head.next = None
+        temp = None
 
 
-class DoublyCircularLinkedList:
-    def __init__(self):
-        self.head = None
-        self.count = 0
+  def reverse(self):
+    curr , prev , next = self.head , None,None
+    while(curr != None):
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+    self.head = prev
 
-    def add_at_tail(self, data) -> bool:
-        # Write code here
+l1 = Link()
+# while(1):
+#   x = int(input())
+#   if(x == 1):
+#     val = int(input())
+#     l1.push(val)
+#   elif(x == 2):
+#     l1.reverse()
+#   elif(x == 3):
+#     l1.pair()
+#   elif(x == 4):
+#     search = int(input())
+#     print(l1.search(search))
 
-    def add_at_head(self, data) -> bool:
-        # Write code here
+for i in range(3):
+  val = int(input())
+  l1.push(val)
 
-    def add_at_index(self, index, data) -> bool:
-        # Write code here
-
-    def get(self, index) -> int:
-        # Write code here
-
-    def delete_at_index(self, index) -> bool:
-        # Write code here
-
-    def get_previous_next(self, index) -> list:
-        # Write code here
-
-
-# Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = []
-iteration_count = 0
-
-for item in input_data.split(', '):
-    inner_list = []
-    if item.isnumeric():
-        data.append(int(item))
-    elif item.startswith('['):
-        item = item[1:-1]
-        for letter in item.split(','):
-            if letter.isnumeric():
-                inner_list.append(int(letter))
-        data.append(inner_list)
-
-obj = DoublyCircularLinkedList()
-result = []
-for i in range(len(operations)):
-    if operations[i] == "add_at_head":
-        result.append(obj.add_at_head(data[i]))
-    elif operations[i] == "add_at_tail":
-        result.append(obj.add_at_tail(data[i]))
-    elif operations[i] == "add_at_index":
-        result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
-    elif operations[i] == "get":
-        result.append(obj.get(data[i]))
-    elif operations[i] == "get_previous_next":
-        result.append(obj.get_previous_next(data[i]))
-    elif operations[i] == 'delete_at_index':
-        result.append(obj.delete_at_index(data[i]))
-
-print(result)
+l1.pair()
+l1.delete_beg()
+l1.pair()
+l1.delete_end()
+l1.pair()
